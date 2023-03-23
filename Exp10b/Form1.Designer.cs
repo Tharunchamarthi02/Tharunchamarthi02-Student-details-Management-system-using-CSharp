62924ff29f05c4b35c9aa9692f01bd414aa09f11
Tharun Chamarthi <tharunchamarthi02@gmail.com>
2023-03-23 22:14:15 +0530

    first commit

---
+++ Exp10b/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Exp10b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lable3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.insertbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.selectbtn = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RegNo";
+            // 
+            // lable3
+            // 
+            this.lable3.AutoSize = true;
+            this.lable3.Location = new System.Drawing.Point(358, 66);
+            this.lable3.Name = "lable3";
+            this.lable3.Size = new System.Drawing.Size(38, 17);
+            this.lable3.TabIndex = 2;
+            this.lable3.Text = "Year";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Branch";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(425, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(170, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(425, 153);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(170, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // insertbtn
+            // 
+            this.insertbtn.Location = new System.Drawing.Point(70, 238);
+            this.insertbtn.Name = "insertbtn";
+            this.insertbtn.Size = new System.Drawing.Size(75, 23);
+            this.insertbtn.TabIndex = 8;
+            this.insertbtn.Text = "Insert";
+            this.insertbtn.UseVisualStyleBackColor = true;
+            this.insertbtn.Click += new System.EventHandler(this.insertbtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(204, 238);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(75, 23);
+            this.deletebtn.TabIndex = 9;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Location = new System.Drawing.Point(332, 238);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(75, 23);
+            this.updatebtn.TabIndex = 10;
+            this.updatebtn.Text = "Update";
+            this.updatebtn.UseVisualStyleBackColor = true;
+            this.updatebtn.Click += new System.EventHandler(this.updatebtn_Click);
+            // 
+            // selectbtn
+            // 
+            this.selectbtn.Location = new System.Drawing.Point(474, 238);
+            this.selectbtn.Name = "selectbtn";
+            this.selectbtn.Size = new System.Drawing.Size(75, 23);
+            this.selectbtn.TabIndex = 11;
+            this.selectbtn.Text = "Select";
+            this.selectbtn.UseVisualStyleBackColor = true;
+            this.selectbtn.Click += new System.EventHandler(this.selectbtn_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(70, 292);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 51;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(512, 217);
+            this.dgv1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 521);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.selectbtn);
+            this.Controls.Add(this.updatebtn);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.insertbtn);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lable3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lable3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button insertbtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.Button selectbtn;
+        private System.Windows.Forms.DataGridView dgv1;
+    }
+}
+
---
+++ Exp10b/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+namespace Exp10b
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=LAPTOP-I8PC1POG; Initial Catalog = studentinfo; Integrated Security = True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void insertbtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(connectionString);
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("insert into studata values(@name,@regno,@year,@branch)", sqlCon);
+            cmd.Parameters.AddWithValue("@name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@regno", int.Parse(textBox2.Text));
+            cmd.Parameters.AddWithValue("@year", int.Parse(textBox3.Text));
+            cmd.Parameters.AddWithValue("@branch", textBox4.Text);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("select * from studata", sqlCon);
+            DataTable dtb1 = new DataTable();
+            sqlDa.Fill(dtb1);
+            dgv1.DataSource = dtb1;
+            sqlCon.Close();
+            MessageBox.Show("Successfully Registered");
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(connectionString);
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("delete studata where regno=@regno", sqlCon);
+            cmd.Parameters.AddWithValue("@regno", int.Parse(textBox2.Text));
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("select * from studata", sqlCon);
+            DataTable dtb1 = new DataTable();
+            sqlDa.Fill(dtb1);
+            dgv1.DataSource = dtb1;
+            sqlCon.Close();
+            MessageBox.Show("Successfully Deleted");
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(connectionString);
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("update studata set name=@name,year=@year,branch=@branch where regno=@regno", sqlCon);
+            cmd.Parameters.AddWithValue("@name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@regno", int.Parse(textBox2.Text));
+            cmd.Parameters.AddWithValue("@year", int.Parse(textBox3.Text));
+            cmd.Parameters.AddWithValue("@branch", textBox4.Text);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("select * from studata", sqlCon);
+            DataTable dtb1 = new DataTable();
+            sqlDa.Fill(dtb1);
+            dgv1.DataSource = dtb1;
+            sqlCon.Close();
+            MessageBox.Show("Successfully Updated");
+
+        }
+
+        private void selectbtn_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("select * from studata", sqlCon);
+                DataTable dtb1 = new DataTable();
+                sqlDa.Fill(dtb1);
+                dgv1.DataSource = dtb1;
+
+            }
+        }
+    }
+}
